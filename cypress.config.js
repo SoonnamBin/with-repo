6df972e09431fe4089af4c7b7dd9b1db07f8e671
soonnambin <soonnam.bin@rsquare.co.kr>
2023-02-15 18:59:41 +0900

    #1

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projectId: "fa3noq",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/e2e/main.cy.js
@@ -0,0 +1,5 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://release-rwith-web-client-stg.dxgx6puil9wtf.amplifyapp.com/')
+  })
+})
