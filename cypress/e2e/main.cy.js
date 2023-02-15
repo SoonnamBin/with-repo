6d7b636696b7fa08fc814839c79cbad192d6b85e
soonnambin <soonnam.bin@rsquare.co.kr>
2023-02-15 19:59:39 +0900

    test#2

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,5 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://release-rwith-web-client-stg.dxgx6puil9wtf.amplifyapp.com/')
+
+    cy.get('.design-cache-prefix-1all1tc').contains('알스퀘어 위드의 시작');
+    
   })
 })
