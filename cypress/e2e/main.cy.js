3a73d02e242e90883107cc53697fbc390e32fe98
soonnambin <soonnam.bin@rsquare.co.kr>
2023-02-16 14:07:56 +0900

    update

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -1,8 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://release-rwith-web-client-stg.dxgx6puil9wtf.amplifyapp.com/')
-
-    cy.get('.design-cache-prefix-1all1tc').contains('알스퀘어 위드의 시작');
-    
+    cy.visit('https://manage-dev.rsquare.co.kr/')
+     
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,40 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// cypress/support/commands.js
+Cypress.Commands.add('loginByGoogleApi', () => {
+    cy.log('Logging in to Google')
+    cy.request({
+      method: 'POST',
+      url: 'https://www.googleapis.com/oauth2/v4/token',
+      body: {
+        grant_type: 'refresh_token',
+        client_id: Cypress.env('357065275436-p3s4fjcjum9cosn4on27fa7jhim1tmsf.apps.googleusercontent.com'),
+        client_secret: Cypress.env('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'),
+        refresh_token: Cypress.env('1//04G551Hy2brBxCgYIARAAGAQSNwF-L9Ir2CBjDq087Oljah0RuCnNGF0f7oDasLNuODV5ztkP-a5nQMVe48puzOgyLc8Y2WokNQ0'),
+      },
+    }).then(({ body }) => {
+      const { access_token, id_token } = body
+  
+      cy.request({
+        method: 'GET',
+        url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+        headers: { Authorization: `Bearer ${access_token}` },
+      }).then(({ body }) => {
+        cy.log(body)
+        const userItem = {
+          token: id_token,
+          user: {
+            googleId: body.sub,
+            email: body.email,
+            givenName: body.given_name,
+            familyName: body.family_name,
+            imageUrl: body.picture,
+          },
+        }
+  
+        window.localStorage.setItem('googleCypress', JSON.stringify(userItem))
+        cy.visit('/')
+      })
+    })
+  })
