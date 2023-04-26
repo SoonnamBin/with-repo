9e1cabd4630de5fe3c03239c1cd45f688996de41
soonnambin <soonnam.bin@rsquare.co.kr>
2023-04-26 14:57:18 +0900

    2023-04-26

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,22 @@
 const { defineConfig } = require('cypress')
 
-// require('dotenv').config()
-
 module.exports = defineConfig({
   projectId: "fa3noq",
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/results",
+    "overwrite": false,
+    "html": true,
+    "json": true,
+    "timestamp": "yyyy-mm-dd HH:MM:ss",
+    "reportFilename": "index",
+    "charts": true,
+    "code": true,
+    "autoOpen": false,
+    "timestampFormat": "{{DATE}} {{TIME}}"
+  },
   e2e: {
-    "baseUrl": "https://release-rwith-web-client-stg.dxgx6puil9wtf.amplifyapp.com/",
+    baseUrl: "https://release-rwith-web-client-stg.dxgx6puil9wtf.amplifyapp.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
